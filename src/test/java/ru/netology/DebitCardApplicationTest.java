cb301756ed970bf1ca177c58720ba4e011a0accd
komisuomi <only-for-serzh@yandex.ru>
2020-11-24 14:37:54 +0300

    build.gradle Modified / Tests Added

--- src/test/java/ru/netology/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/DebitCardApplicationTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,7 +11,8 @@ public class DebitCardApplicationTest {
     @Test
     void shouldConfirmRequest() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Шамиль Газизов");
+        //SelenideElement form = $("[id=root]");
+        $("[data-test-id='name'] input").setValue("Шамиль Газизов");
         $("[data-test-id=phone] input").setValue("+79005553535");
         $("[data-test-id=agreement]").click();
         $("button").click();
@@ -18,11 +20,32 @@ public class DebitCardApplicationTest {
     }
 
     @Test
-    void shouldNotConfirmRequestIfInvalidName() {
+    void shouldNotConfirmRequestIfIncorrectName() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Shamil Gazizov");
         $("[data-test-id=phone] input").setValue("+79005553535");
         $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        //$("input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotConfirmRequestIfIncorrectPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Сергей Иванов");
+        $("[data-test-id=phone] input").setValue("9042222222");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input__sub").shouldHave(exactText("Укажите точно как в паспорте"));
+    }
+
+    @Test
+    void shouldNotConfirmRequestIfIncorrectAll() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Sergey Ivanov");
+        $("[data-test-id=phone] input").setValue("9042222222");
+        $("[data-test-id=agreement]").exists();
         $("button").click();
         $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
